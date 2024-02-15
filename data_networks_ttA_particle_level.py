08c91314dda0560d9fec918fdd383644df9b4dd1
Sergio Sanchez Cruz <sergio.sanchez.cruz@cern.ch>
2024-02-15 10:31:51 +0100

    Pushing ttA

---
+++ data_networks_ttA_particle_level.py
@@ -0,0 +1,70 @@
+# python train.py --name ttbar_particle_level --data-path /work/sesanche/ttH/UL/CMSSW_10_4_0/src/CMGTools/TTHAnalysis/macros/kk     --analysis ttbar_pl
+
+from torch.utils.data import Dataset, IterableDataset
+import torch.nn as nn
+import glob 
+import torch 
+import uproot 
+
+class dataset( IterableDataset ):
+    def __init__( self, path, device):
+        self.files=glob.glob(path)
+        self.device=device
+        self.length=0
+        for fil in self.files:
+            with uproot.open(fil) as f:
+                self.length+=f['Friends'].num_entries
+
+    def __len__(self):
+        return self.length
+
+    def __iter__(self):
+        for fil in self.files:
+            with uproot.open(fil) as f:
+                tree = f['Friends']
+                control_vars=torch.Tensor( tree.arrays(["weight_ctzi_sm"],library='pd').values ).to(self.device)
+                weights  =torch.Tensor(tree.arrays(["weight_sm", "weight_ctzi_sm"], library='pd').values).to(self.device)
+                variables=torch.Tensor(tree.arrays(['lep_top_px', 'lep_top_py', 'lep_top_pz',
+                                                    'random_b1_px', 'random_b1_py', 'random_b1_pz',
+                                                    'random_b2_px', 'random_b2_py', 'random_b2_pz',
+                                                    'random_j1_px', 'random_j1_py', 'random_j1_pz',
+                                                    'random_j2_px', 'random_j2_py', 'random_j2_pz',
+                                                    'lep_top_sign',
+                                                    'met_px', 'met_py',
+                                                    'photon_px', 'photon_py', 'photon_pz',
+
+                                                ],library='pd').values).to(self.device)
+            yield from zip(weights, control_vars, variables)
+
+
+
+class network(nn.Module):
+    def __init__(self, device):
+        super().__init__()
+        self.main_module = nn.Sequential( 
+            nn.Linear(21,80),
+            nn.LeakyReLU(),
+            nn.Linear(80, 80),
+            nn.LeakyReLU(),
+            nn.Linear(80, 40),
+            nn.LeakyReLU(),
+            nn.Linear(40, 20),
+            nn.LeakyReLU(),
+            nn.Linear(20, 1 ),
+        )
+        self.main_module.to(device)
+
+    def forward(self, x):
+        
+        cpx= torch.stack(
+            [-x[:,0], -x[:,1] , -x[:,2],   # -lep 
+             -x[:,6], -x[:,7],  -x[:,8],   # -b2 # this shouldnt add information but ok 
+             -x[:,3], -x[:,4] , -x[:,5],   # -b1 
+             -x[:,12], -x[:,13] , -x[:,14],   # -j2
+             -x[:,9], -x[:,10] , -x[:,11],   # -j1
+             -x[:,15], # -lep top sign
+             -x[:,16],-x[:,17],  # -met
+             -x[:,18],-x[:,19],-x[:,20], # -photon
+             ],dim=1)
+
+        return self.main_module(x)-self.main_module(cpx)
--- train.py
+++ train.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     parser.add_argument("--prefetch"      ,  type=str  , default=None, help="Temporary directory to prefetch data")
     parser.add_argument("--data-format"   ,  type=str  , default='h5', help="Extension of input files")
     parser.add_argument("--data-path"     , type=str, default="/pnfs/psi.ch/cms/trivcat/store/user/sesanche/CP_equivariant/ttbar/ntuples", help="Path of the input dataset")
-    parser.add_argument("--analysis"     , type=str, default="ttbar", choices=['ttbar','ttbar_ideal','ttbar_withneutrinos', 'ttbb_godmode', 'ttZ_3l','ttZ_3l_v2','ttA_1l','ttW', 'ttbar_pl', 'ww'], help="Analysis to run, defines dataset type and neural network")
+    parser.add_argument("--analysis"     , type=str, default="ttbar", choices=['ttbar','ttbar_ideal','ttbar_withneutrinos', 'ttbb_godmode', 'ttZ_3l','ttZ_3l_v2','ttA_1l','ttW', 'ttbar_pl','ttA_pl', 'ww'], help="Analysis to run, defines dataset type and neural network")
 
 
     args = parser.parse_args()
@@ -48,6 +48,8 @@ if __name__ == "__main__":
         max_value=0.05
     elif args.analysis == 'ttbar_pl':
         from data_networks_ttbar_particle_level import dataset, network
+    elif args.analysis == 'ttA_pl':
+        from data_networks_ttA_particle_level import dataset, network
     elif args.analysis == 'ww':
         from data_networks_ww import dataset, network
 
