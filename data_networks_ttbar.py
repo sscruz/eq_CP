b58fb50eb8b52111c9d19bf885fbddda9a32cce9
Santi <santiagovilad@gmail.com>
2024-06-14 18:09:05 +0200

    correcciones

--- data_networks_ttbar.py
+++ data_networks_ttbar.py
@@ -21,6 +21,7 @@ class dataset( IterableDataset ):
     def __iter__(self):
         for f in self.files:
             thedata=pd.read_hdf(f, 'df')
+            print(thedata.columns)
             control_vars=torch.Tensor( thedata[['control_cnr_crn','control_cnk_kn','control_rk_kr']].values).to(self.device)
             weights  =torch.Tensor(thedata[["weight_sm", "weight_lin",
                                              "weight_quad"]].values).to(self.device)
